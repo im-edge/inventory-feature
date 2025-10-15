61aaed472536123ec240eeee57849bf72549391f
Thomas Gelf <thomas@gelf.net>
2025-10-15 15:39:07 +0200

    CredentialLoader: split lines

--- src/CredentialLoader.php
+++ src/CredentialLoader.php
@@ -9,8 +9,12 @@ class CredentialLoader
 {
     public static function fetchAllForDataNode(UuidInterface $uuid, DbConnection $db): array
     {
-        $sql1 = 'SELECT DISTINCT c.* FROM snmp_credential c JOIN snmp_agent a ON a.credential_uuid = c.credential_uuid WHERE a.datanode_uuid = ' . self::escapeBinary($uuid->getBytes());
-        $sql2 = 'SELECT DISTINCT c.* FROM snmp_credential c JOIN snmp_discovery_candidate dc ON dc.credential_uuid = c.credential_uuid WHERE dc.datanode_uuid = ' . self::escapeBinary($uuid->getBytes());
+        $sql1 = 'SELECT DISTINCT c.* FROM snmp_credential c'
+            . ' JOIN snmp_agent a ON a.credential_uuid = c.credential_uuid'
+            . ' WHERE a.datanode_uuid = ' . self::escapeBinary($uuid->getBytes());
+        $sql2 = 'SELECT DISTINCT c.* FROM snmp_credential c'
+            . ' JOIN snmp_discovery_candidate dc ON dc.credential_uuid = c.credential_uuid'
+            . ' WHERE dc.datanode_uuid = ' . self::escapeBinary($uuid->getBytes());
         $sql = "SELECT * FROM ($sql1 UNION ALL $sql2) sub GROUP BY credential_uuid";
 
         return $db->fetchAll($sql);
@@ -18,8 +22,10 @@ class CredentialLoader
 
     public static function fetchAll(DbConnection $db): array
     {
-        $sql1 = 'SELECT DISTINCT a.datanode_uuid, c.* FROM snmp_credential c JOIN snmp_agent a ON a.credential_uuid = c.credential_uuid';
-        $sql2 = 'SELECT DISTINCT dc.datanode_uuid, c.* FROM snmp_credential c JOIN snmp_discovery_candidate dc ON dc.credential_uuid = c.credential_uuid';
+        $sql1 = 'SELECT DISTINCT a.datanode_uuid, c.* FROM snmp_credential c'
+            . ' JOIN snmp_agent a ON a.credential_uuid = c.credential_uuid';
+        $sql2 = 'SELECT DISTINCT dc.datanode_uuid, c.* FROM snmp_credential c'
+            . ' JOIN snmp_discovery_candidate dc ON dc.credential_uuid = c.credential_uuid';
         $sql = "SELECT * FROM ($sql1 UNION ALL $sql2) sub GROUP BY datanode_uuid, credential_uuid";
 
         return $db->fetchAll($sql);
